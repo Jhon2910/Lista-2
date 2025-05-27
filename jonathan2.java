0bacac878a71e519872b4b6c0335b6fc563bbafa
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-05-27 08:42:21 -0300

    Update jonathan2.java

--- jonathan2.java
+++ jonathan2.java
@@ -3,7 +3,7 @@ public class jonathan2{
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digíte o primeiro valor:");
-        int num1 = (int) teclado.nextDouble();
+        int num1 = (int) teclado.nextInt();
         int antecessor = num1 - 1;
         int sucessor = num1 + 1;
         System.out.println("sucessor:" +sucessor);
