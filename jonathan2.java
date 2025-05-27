b68459df6c6f3e75893a9f346dbaa5ec3c38c54f
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-05-27 08:40:07 -0300

    Create jonathan2.java

---
+++ jonathan2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class jonathan2{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digíte o primeiro valor:");
+        int num1 = (int) teclado.nextDouble();
+        int antecessor = num1 - 1;
+        int sucessor = num1 + 1;
+        System.out.println("sucessor:" +sucessor);
+        System.out.println("antecessor:" +antecessor);
+    teclado.close();
+        }
+    }
