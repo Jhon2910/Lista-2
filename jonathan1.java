baf62cac1b64f34ae28db5d2070f2093f1961f1c
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-05-28 10:43:26 -0300

    Update jonathan1.java

--- jonathan1.java
+++ jonathan1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class jonathan1{
+class jonathan1{
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
@@ -7,9 +7,9 @@ public class jonathan1{
         double num1 = teclado.nextDouble();
         System.out.println("Digíte o segundo valor:");
         double num2 = teclado.nextDouble();
-        double soma = Math.pow(num1, 2) + Math.pow(num2, 2);
-        double media = Math.sqrt(soma);
+        double soma = num1 + num2;
+        double media = soma / 2;
         System.out.printf("A média dos números é :%.2f%n",media);
-    teclado.close();
-        }
+        teclado.close();
     }
+}
