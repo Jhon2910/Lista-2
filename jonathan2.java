632bbffc84da6d95ca0dea184c46d13b9b2b2f0b
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-05-28 16:55:23 -0300

    Update jonathan2.java

--- jonathan2.java
+++ jonathan2.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class jonathan2{
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Digíte o primeiro valor:");
-        int num1 = (int) teclado.nextInt();
-        int antecessor = num1 - 1;
-        int sucessor = num1 + 1;
-        System.out.println("sucessor:" +sucessor);
-        System.out.println("antecessor:" +antecessor);
-    teclado.close();
-        }
-    }
+   public static void main(String[] args) {
+      Scanner teclado = new Scanner(System.in);
+      System.out.print("Digíte o valor:");
+      int num1 = teclado.nextInt();
+      int antecessor = num1 - 1;
+      int sucessor = num1 + 1;
+      System.out.printf("sucessor: %d",sucessor);
+      System.out.printf("\nantecessor: %d",antecessor);
+      teclado.close();
+   }
+}
