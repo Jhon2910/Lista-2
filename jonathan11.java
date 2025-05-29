035c8839c00f619f59a3f7d48975bdc3ddfcec67
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-05-29 08:45:12 -0300

    Create jonathan11.java

---
+++ jonathan11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+    class jonathan11 {
+        public static class void main(String[]args){
+            Scanner teclado = new Scanner(System.in);
+            
+            System.out.print("Digite seu salário: ");
+            double salario = teclado.nextDouble();
+            
+            System.out.print("Digite o valor da conta1");
+            double conta1 = teclado.nextDouble();
+            
+            System.out.print("Digite o valor da conta2");
+            double conta2 = teclado.nextDouble();
+            
+            double Juros = (conta1 + conta2) * 0.2;
+            double resto = salario -  
+        
+        
+        }
+    }
