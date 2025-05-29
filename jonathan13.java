3b6f2b607eb2462aa948b2ce4a454ffa963c679f
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-05-29 16:53:05 -0300

    Update jonathan13.java

--- jonathan13.java
+++ jonathan13.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
         public static void main(String[] args) {
             Scanner teclado = new Scanner(System.in);
             System.out.print("Ano : ");
+            
             int Ano = teclado.nextInt();
             int CopasRealizadas = ((Ano - 1930)/4)+1-2;
+            
             System.out.printf("O total de copas do mundo realizadas neste periodo e de : %d",CopasRealizadas);
+            teclado.close();
         }
     }
