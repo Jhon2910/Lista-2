0159e3bd7c550ef28b886b77b587a8f9c0835c5e
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-03 20:50:57 -0300

    Update jonathan13.java

--- jonathan13.java
+++ jonathan13.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
-    class jonathan13 {
-        public static void main(String[] args) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.print("Ano : ");
-            
-            int Ano = teclado.nextInt();
-            int CopasRealizadas = ((Ano - 1930)/4)+1;
-            
-            System.out.printf("O total de copas do mundo realizadas neste periodo e de : %d",CopasRealizadas);
-            teclado.close(); //ainda nao acabei
+class jonathan13 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o ano : ");
+
+        int Ano = teclado.nextInt();
+
+        if (Ano < 1930 || Ano > 2025){
+            System.out.print("Digite um ano entre 1930 e 2025! ");
+        }
+        else {
+            int CopasRealizadas = ((Ano - 1930)/4);
+
+        if (Ano >= 1942 || Ano <= 1946){
+            System.out.print(CopasRealizadas - 1);
+        }
+        else{
+            System.out.printf("Foram realizadas %d copas do mundo",CopasRealizadas );//nao terminei
+            }
         }
     }
+
+}
